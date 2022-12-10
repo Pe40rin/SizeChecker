5f1bddd1146c996cde1675a3ab1ae8ea7749dc2b
Pe40rin <gxikszb@gmail.com>
2022-12-10 11:44:19 +0300

    Add project

---
+++ SizeChecker/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeChecker
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Input X size(number):");
+            int x = int.Parse(Console.ReadLine());
+
+            if (InputChecker(x) == 0)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Input Y size(number):");
+            int y = int.Parse(Console.ReadLine());
+
+            if (InputChecker(y) == 0)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Input Z size(number):");
+            int z = int.Parse(Console.ReadLine());
+
+            if (InputChecker(z) == 0)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Input hole A size(number):");
+            int a = int.Parse(Console.ReadLine());
+
+            if (InputChecker(a) == 0)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Input hole A size(number):");
+            int b = int.Parse(Console.ReadLine());
+
+            if (InputChecker(b) == 0)
+            {
+                return 0;
+            }
+
+            SizeCheck(x, y, z, a, b);
+
+            return 1;
+        }
+
+        static int InputChecker(int input)
+        {
+            if (input <= 0)
+            {
+                Console.WriteLine("Input size can't be less or equal zero!");
+                return 0;
+            }
+
+            else
+            {
+                return 1;
+            }
+        }
+
+        static void SizeCheck(int x, int y, int z, int a, int b)
+        {
+            
+        }
+    }
+}
